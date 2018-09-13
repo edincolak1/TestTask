50fb3b9ff81f63aa14036539f29c69025c3de887
edin <edin@1sales.io>
2018-09-13 17:09:08 +0200

    new

--- app/Transformers/StageTransformer.php
+++ app/Transformers/StageTransformer.php
@@ -17,14 +17,9 @@ class StageTransformer extends TransformerAbstract
         return [
             'identifier' => (int)$Stage->id,
             'name of the stage' => (string)$Stage->name,
-<<<<<<< HEAD
             'order in the stage' => (int)$Stage->order,
             'belongs to board' => (int)$Stage->board_id,
             'completed' => (boolean)$Stage->completed,
-=======
-            'order of the stage' => (int)$Stage->order,
-            'belongs to board' => (int)$Stage->board_id,
->>>>>>> 840a63e40ec6bc6b94c7ea7b883a9202f25c0c34
             'stageCreated' => (string)$Stage->created_at,
             'stageLastChange' => (string)$Stage->updated_at,         
         ];
